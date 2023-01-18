223604fabeef74ad6fad407fcdc97b08a0dae6b1
Matthew Hawkes <82766547+mh0w@users.noreply.github.com>
2023-01-18 11:42:52 +0000

    Add files via upload

---
+++ extra_sites_list.py
@@ -0,0 +1,10 @@
+"""Add more sites to the Multisite Search Tool."""
+
+
+def create_extra_sites_list():
+    """Create dictionary of site names and URLs."""
+    extra_sites_list = {
+        "Bing": "https://www.bing.com/search?q=SEARCH_TEXT_HERE",
+    }
+
+    return extra_sites_list
